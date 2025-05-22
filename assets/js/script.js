8be9dab5b5d9421a3f1bd701a27863db45efd459
Ri4yy <maksimov.1913@list.ru>
2025-05-22 11:55:07 +0300

    upd

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,19 +1,22 @@
 document.addEventListener('DOMContentLoaded', () => {
-    let menu = document.querySelector('.menu'),
-    btnMenu = document.querySelector('.btn-menu');
+    let menu = document.querySelector('.header-mobile'),
+    btnMenu = document.querySelector('.btn-menu'),
+    html = document.querySelector('html');
 
     btnMenu.addEventListener('click', (e) => {
-        menu.classList.toggle('menu--open')
+        menu.classList.toggle('open')
 
         btnMenu.classList.toggle('btn-menu--open')
+        html.classList.toggle('no-scroll')
     })
 
     function resize() {
         let width = window.innerWidth;
 
-        if (width > 768) {
-            menu.classList.remove('menu--open')
+        if (width > 1280) {
+            menu.classList.remove('open')
             btnMenu.classList.remove('btn-menu--open')
+            html.classList.remove('no-scroll')
         } else {
             return
         }
@@ -23,4 +26,51 @@ document.addEventListener('DOMContentLoaded', () => {
         resize()
     })
     resize()
+
+    let dropdownEvent = document.querySelector('#dropdown-events'),
+        dropdownContainer = document.querySelector('.request-form__dropdown'),
+        dropdownItem = document.querySelectorAll('.request-form__dropdown-item'),
+        dropdownSelected = document.querySelector('#dropdown-events span'),
+        inputEvent = document.querySelector('#selected-event')
+
+    dropdownEvent.addEventListener('click', () => {
+        dropdownEvent.closest('.request-form__dropdown').classList.toggle('open')
+    })
+
+    dropdownItem.forEach(el => {
+        el.addEventListener('click', () => {
+            let eventName = el.textContent;
+            
+            eventUpdate(eventName)
+        })
+    });
+
+    function eventUpdate(name) {
+        inputEvent.value = name;
+        dropdownSelected.innerHTML = name;
+        dropdownContainer.classList.remove('open')
+    }
+
+    // Мероприятия
+    const wrapper = document.querySelector('.calendar__wrapper');
+    const itemButtons = wrapper.querySelectorAll('.calendar__item-btn');
+    const eventDetail = wrapper.querySelector('.event-detail');
+    const details = wrapper.querySelector('.calendar__detail');
+  
+    itemButtons.forEach(button => {
+      button.addEventListener('click', () => {
+        details.style.display = 'block';
+        eventDetail.classList.toggle('open')
+      });
+    });
+
+    // О клубе
+    let aboutBtn = document.querySelectorAll('.about__item-btn');
+    const aboutDetail = document.querySelector('#about-detail');
+
+    aboutBtn.forEach(btn => {
+        btn.addEventListener('click', () => {
+            aboutDetail.classList.toggle('open')
+        })
+    })
 })
--- assets/js/swiper.js
+++ assets/js/swiper.js
@@ -1,72 +1,143 @@
-const swiper = new Swiper('.swiper', {
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     spaceBetween: 20,
+//     enabled: true,
+
+//     breakpoints: {
+//         320: {
+//             allowTouchMove: true,
+//             slidesPerView: 1,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         480: {
+//             allowTouchMove: true,
+//             slidesPerView: 2,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         768: {
+//             allowTouchMove: true,
+//             slidesPerView: 3,
+//             grid: {
+//                 rows: 1,
+//                 fill: "row",
+//             }
+//         },
+//         1024: {
+//             allowTouchMove: true,
+//             slidesPerView: 2,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//         1280: {
+//             allowTouchMove: true,
+//             slidesPerView: 3,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//         1921: {
+//             allowTouchMove: true,
+//             slidesPerView: 4,
+//             grid: {
+//                 rows: 2,
+//                 fill: "row",
+//             }
+//         },
+//     },
+  
+//     // If we need pagination
+//     pagination: {
+//       el: '.swiper-pagination',
+//     },
+  
+//     // Navigation arrows
+//     navigation: {
+//       nextEl: '.swiper-button-next',
+//       prevEl: '.swiper-button-prev',
+//     },
+  
+//     // And if we need scrollbar
+//     scrollbar: {
+//       el: '.swiper-scrollbar',
+//     },
+// });
+
+const swiperClub = new Swiper('.swiper-club', {
     // Optional parameters
     spaceBetween: 20,
+    slidesPerView: 1,
     enabled: true,
-
-    breakpoints: {
-        320: {
-            allowTouchMove: true,
-            slidesPerView: 1,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        480: {
-            allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        768: {
-            allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 1,
-                fill: "row",
-            }
-        },
-        1024: {
-            allowTouchMove: true,
-            slidesPerView: 2,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1280: {
-            allowTouchMove: true,
-            slidesPerView: 3,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-        1921: {
-            allowTouchMove: true,
-            slidesPerView: 4,
-            grid: {
-                rows: 2,
-                fill: "row",
-            }
-        },
-    },
   
     // If we need pagination
     pagination: {
-      el: '.swiper-pagination',
+      el: '.swiper-club__pagination',
     },
   
     // Navigation arrows
     navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  
-    // And if we need scrollbar
-    scrollbar: {
-      el: '.swiper-scrollbar',
+      nextEl: '.club__swiper-btn--next',
+      prevEl: '.club__swiper-btn--prev',
     },
 });
+const swiperGallery = new Swiper('.swiper-gallery', {
+    // Optional parameters
+    spaceBetween: 20,
+    slidesPerView: 1,
+    enabled: true,
+  
+    // If we need pagination
+    pagination: {
+      el: '.swiper-gallery__pagination',
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.gallery__swiper-btn--next',
+      prevEl: '.gallery__swiper-btn--prev',
+    },
+});
+
+var swiperTimeline = new Swiper(".swiper-timeline", {
+    autoHeight: true,
+    autoplay: {
+      delay: 5000,
+      disableOnInteraction: false
+    },
+    speed: 500,
+    direction: "horizontal",
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev"
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      type: "progressbar"
+    },
+    loop: false,
+    effect: "slide",
+    spaceBetween: 30,
+    on: {
+      init: function () {
+        $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+        $(".swiper-pagination-custom .swiper-pagination-switch").eq(0).addClass("active");
+      },
+      slideChangeTransitionStart: function () {
+        $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+        $(".swiper-pagination-custom .swiper-pagination-switch").eq(swiperTimeline.realIndex).addClass("active");
+      }
+    }
+  });
+  $(".swiper-pagination-custom .swiper-pagination-switch").click(function () {
+    swiperTimeline.slideTo($(this).index());
+    $(".swiper-pagination-custom .swiper-pagination-switch").removeClass("active");
+    $(this).addClass("active");
+  });
